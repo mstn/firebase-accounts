1518ec3ae53da1a07dd9674376cd345d9a34e482
mstn <mstn@git.com>
2015-11-15 14:46:11 +0100

    simple oauth popup auth

---
+++ firebase_accounts_client.js
@@ -0,0 +1,55 @@
+"use strict"
+
+class FirebaseAccounts {
+  constructor(options){
+    this.ref = null;
+    // client only collection to store users
+    this.users = new Mongo.Collection(null); 
+  }
+  userId(){
+    const auth = this.connection().getAuth();
+    return auth ? auth.uid : null;
+  }
+  user(){
+    const userId = this.userId();
+    return this.users.findOne(userId);
+  }
+  config(options){
+    options = options || {};
+
+    const endpoint = options.endpoint;
+    
+    if (!endpoint){
+      throw new Error('Firebase endpoint is required.');
+    }
+
+    this.ref = new Firebase._FirebaseRef(endpoint);
+  }
+  logout(){
+    this.connection().unauth();  
+  }
+  login(serviceType, callback){
+    this.connection().authWithOAuthPopup(serviceType,
+                                  handleOAuthPopup(this, callback));
+  }
+  connection(){
+    if (!this.ref){
+      throw new Error('No Firebase endpoint defined.');
+    }
+    return this.ref;
+  }
+};
+
+const handleOAuthPopup = function(accounts, callback){
+  return function(error, authData){
+    if (error){
+      callback(error);
+    } else {
+      authData._id = authData.uid;
+      accounts.users.insert(authData);
+      callback(null, authData);
+    }
+  }
+};
+
+Firebase.Accounts = new FirebaseAccounts;
---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  summary:'client side authentication using Firebase',
+  version:'0.0.1',
+  name:'mstn:firebase-accounts'
+});
+
+Package.onUse(function(api){
+  api.versionsFrom('1.2');
+
+  api.use([
+    'mstn:firebase-core',
+    'mongo'
+  ], 'client');
+
+  api.addFiles([
+    'firebase_accounts_client.js'
+  ], 'client');
+
+});
